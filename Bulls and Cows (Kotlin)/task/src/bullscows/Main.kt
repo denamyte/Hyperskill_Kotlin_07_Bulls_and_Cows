8ba72e7b72e4abd22369186f55f3087d20a1e701
denys <denamyte@gmail.com>
2023-09-02 23:39:15 +0300

    2023-09-02: Stage 4/7

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -3,12 +3,24 @@ package bullscows
 private const val digits = "0123456789"
 
 fun main() {
-//    val grader = Grader(code)
-//    val sample = readln()
-//    val barnyard = grader.grade(sample)
-//    buildMessage(barnyard).run(::println)
+    val number = inputNumber()
+    println("Okay, let's start a game!")
+    val code = generateCode(number)
+    val grader = Grader(code)
+    var turns = 0
+    var bulls = 0
 
-    val number = readln().toInt()
+    while (bulls != number) {
+        turns++
+        println("Turn $turns:")
+        val sample = readln()
+        val barnyard = grader.grade(sample)
+        buildMessage(barnyard).run(::println)
+        bulls = barnyard.first
+    }
+
+    println("Congratulations! You guessed the secret code.")
+
     println(
         when {
             number > 10 -> "Error: bla-bla..."
@@ -17,21 +29,30 @@ fun main() {
     )
 }
 
+fun inputNumber(): Int {
+    while (true) {
+        println("Please, enter the secret code's length:")
+        val number = readln().toInt()
+        if (number > 10)
+            println("Error: bla-bla...")
+        else return number
+    }
+}
+
 fun generateCode(number: Int): String {
     var dList = digits.toList()
     while (dList.first() == '0') dList = dList.shuffled()
     return dList.subList(0, number).joinToString("")
 }
 
-//fun buildMessage(res: Pair<Int, Int>): String {
-//    val sb = StringBuilder("Grade: ")
-//    val first = res.first > 0
-//    val second = res.second > 0
-//    if (first) sb.append("${res.first} bull(s)")
-//    if (first && second) sb.append(" and ")
-//    if (second) sb.append("${res.second} cow(s)")
-//    if (!first && !second) sb.append("None")
-//    sb.append(". The secret code is $code.")
-//    return sb.toString()
-//}
+fun buildMessage(res: Pair<Int, Int>): String {
+    val sb = StringBuilder("Grade: ")
+    val first = res.first > 0
+    val second = res.second > 0
+    if (first) sb.append("${res.first} bull(s)")
+    if (first && second) sb.append(" and ")
+    if (second) sb.append("${res.second} cow(s)")
+    if (!first && !second) sb.append("None")
+    return sb.toString()
+}
 
